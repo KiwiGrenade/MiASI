ceea70af6fe19d6a0c769615136ce0d2c1c51445
jaskow <jakub.jaskow@nokia.com>
2025-03-27 17:30:20 +0100

    Add lab3
    Add compiler with:
    - basic arithmetics (+, -, *, /)
    - integers
    - variables

--- lab3/src/compiler/EmitVisitor.java
+++ lab3/src/compiler/EmitVisitor.java
@@ -1,13 +1,18 @@
 package compiler;
 
+import SymbolTable.LocalSymbols;
+import grammar.firstLexer;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 import grammar.firstBaseVisitor;
 import grammar.firstParser;
 
+import java.util.Objects;
+
 public class EmitVisitor extends firstBaseVisitor<ST> {
     private final STGroup stGroup;
+    LocalSymbols<Integer> localVars = new LocalSymbols<Integer>();
 
     public EmitVisitor(STGroup group) {
         super();
@@ -27,10 +32,10 @@ public class EmitVisitor extends firstBaseVisitor<ST> {
     }
 
 
-    @Override
-    public ST visitTerminal(TerminalNode node) {
-        return new ST("Terminal node:<n>").add("n",node.getText());
-    }
+//    @Override
+//    public ST visitTerminal(TerminalNode node) {
+//        return new ST("Terminal node:<n>").add("n",node.getText());
+//    }
 
     @Override
     public ST visitInt_tok(firstParser.Int_tokContext ctx) {
@@ -41,7 +46,90 @@ public class EmitVisitor extends firstBaseVisitor<ST> {
 
     @Override
     public ST visitBinOp(firstParser.BinOpContext ctx) {
-        ST st = stGroup.getInstanceOf("dodaj");
-        return st.add("p1",visit(ctx.l)).add("p2",visit(ctx.r));
+        String opAss = "";
+
+        ST left = visit(ctx.l);
+        ST right = visit(ctx.r);
+
+        switch (ctx.op.getType()) {
+            case firstLexer.ADD -> opAss = "add";
+            case firstLexer.SUB -> opAss = "sub";
+            case firstLexer.MUL -> opAss = "mul";
+            case firstLexer.DIV -> {
+                opAss = "div";
+//                if (right == 0) {
+//                    throw new ArithmeticException("Dzielenie przez zero");
+//                }
+//                yield left / right;
+            }
+//            case firstLexer.GE -> opAss = "ge";
+//            case firstLexer.LE -> opAss = "le";
+//            case firstLexer.EQ-> opAss = "eq";
+//            case firstLexer.GEQ -> opAss = "geq";
+//            case firstLexer.LEQ -> opAss = "leq";
+//            case firstLexer.NEQ-> opAss = "neq";
+//            case firstLexer.OR-> opAss = "or";
+//            case firstLexer.AND-> opAss = "and";
+            default -> throw new RuntimeException("Nieznany operator: " + ctx.op.getText());
+        }
+
+        ST st = stGroup.getInstanceOf(opAss);
+
+        return st.add("p1",left).add("p2",right);
+    }
+
+    @Override
+    public ST visitVar_tok(firstParser.Var_tokContext ctx) {
+        ST st = stGroup.getInstanceOf("read");
+        return st.add("n", ctx.getText());
+    }
+
+    @Override
+    public ST visitPars(firstParser.ParsContext ctx) {
+        return visit(ctx.expr());
+    }
+
+    @Override
+    public ST visitAssign(firstParser.AssignContext ctx) {
+        ST st = stGroup.getInstanceOf("assign");
+        st.add("name", ctx.ID().getText());
+        st.add("expr", visit(ctx.expr()));
+        return st;
+    }
+
+    @Override
+    public ST visitPrint_stat(firstParser.Print_statContext ctx) {
+        ST st = stGroup.getInstanceOf("deflt");
+        return st.add("elem", visit(ctx.expr()));
+    }
+
+    @Override
+    public ST visitExpr_stat(firstParser.Expr_statContext ctx) {
+        return super.visitExpr_stat(ctx);
+    }
+
+    @Override
+    public ST visitProg(firstParser.ProgContext ctx) {
+        ST st = stGroup.getInstanceOf("end");
+        return st.add("prog", visitChildren(ctx));
+    }
+
+    @Override
+    public ST visitDeclare_stat(firstParser.Declare_statContext ctx) {
+        return super.visitDeclare_stat(ctx);
+    }
+
+    @Override
+    public ST visitDeclare_expr(firstParser.Declare_exprContext ctx) {
+        String varName = ctx.ID().getText();
+        if(ctx.ASSIGN() != null) {
+            ST st = stGroup.getInstanceOf("init");
+            st.add("name", varName);
+            return st.add("expr", visit(ctx.expr()));
+        }
+        else {
+            ST st = stGroup.getInstanceOf("dec");
+            return st.add("n", varName);
+        }
     }
 }
